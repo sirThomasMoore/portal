407ed241b4e00f201fccbdc8e6e3a0f6ae096e88
Joshua S. Eagle <joshuaseagle@gmail.com>
2021-10-10 12:38:55 -0400

    issue #7: Added username and email

--- ng-app/src/app/app.module.ts
+++ ng-app/src/app/app.module.ts
@@ -16,41 +16,49 @@ import { AuthService } from './services/auth.service';
 import { ConfigService } from './services/config.service';
 import { HeaderComponent } from './components/header/header.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { MatFormFieldDefaultOptions, MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
+const appearance: MatFormFieldDefaultOptions = {
+	appearance: 'fill'
+};
 @NgModule({
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-    LoginComponent,
-    RegisterComponent,
-    UserManagementComponent,
-    UserProfileComponent,
-    HeaderComponent,
-    NavigationComponent,
-  ],
-  exports: [],
-  imports: [
-    AppRoutingModule,
-    BrowserAnimationsModule,
+	declarations: [
+		AppComponent,
+		DashboardComponent,
+		LoginComponent,
+		RegisterComponent,
+		UserManagementComponent,
+		UserProfileComponent,
+		HeaderComponent,
+		NavigationComponent,
+	],
+	exports: [],
+	imports: [
+		AppRoutingModule,
+		BrowserAnimationsModule,
 		BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    MaterialModule,
-    ReactiveFormsModule
-  ],
-  providers: [
-    AuthService,
-    ConfigService,
-    { provide: APP_BASE_HREF, useValue: '/' },
-    {
-      provide: APP_INITIALIZER,
-      useFactory: (configService: ConfigService) => () =>
-        configService.loadConfigData().toPromise(),
-      deps: [ConfigService],
-      multi: true
-    },
-    httpInterceptorProviders,
-  ],
-  bootstrap: [AppComponent]
+		FormsModule,
+		HttpClientModule,
+		MaterialModule,
+		ReactiveFormsModule
+	],
+	providers: [
+		AuthService,
+		ConfigService,
+		{ provide: APP_BASE_HREF, useValue: '/' },
+		{
+			provide: APP_INITIALIZER,
+			useFactory: (configService: ConfigService) => () =>
+				configService.loadConfigData().toPromise(),
+			deps: [ConfigService],
+			multi: true
+		},
+		{
+			provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+			useValue: appearance
+		},
+		httpInterceptorProviders,
+	],
+	bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
